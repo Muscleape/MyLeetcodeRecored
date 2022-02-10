3fa8a01fbe6a82fe70bb3be3b347d3e2ab89aa01
Muscleape <Muscleape529@gmail.com>
2022-02-10 09:38:44 +0800

    init

---
+++ src/main/java/com/muscleape/myleetcoderecored/MyLeetcodeRecoredApplication.java
@@ -0,0 +1,13 @@
+package com.muscleape.myleetcoderecored;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLeetcodeRecoredApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLeetcodeRecoredApplication.class, args);
+    }
+
+}
