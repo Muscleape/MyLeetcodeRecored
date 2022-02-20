2e4fab18134218f7503a0b0f25006cf6c980486b
Muscleape <Muscleape529@gmail.com>
2022-02-20 16:06:43 +0800

    增加题目描述

--- src/main/java/com/muscleape/myleetcoderecored/offer/Code05.java
+++ src/main/java/com/muscleape/myleetcoderecored/offer/Code05.java
@@ -5,6 +5,21 @@ package com.muscleape.myleetcoderecored.offer;
  * <p>
  * https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
  *
+ * 剑指 Offer 05. 替换空格
+ * 请实现一个函数，把字符串 s 中的每个空格替换成"%20"。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ * 输入：s = "We are happy."
+ * 输出："We%20are%20happy."
+ *
+ *
+ * 限制：
+ *
+ * 0 <= s 的长度 <= 10000
+ *
  * @author muscleape
  * @date 2022/02/14 09:33
  **/
